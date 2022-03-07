1d37f9af1a1d153701582ce80c39186b968e916d
ShinHyoJung <59465978+ShinHyoJung@users.noreply.github.com>
2022-03-08 00:10:34 +0900

    Update Application.java

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,6 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
@@ -13,6 +12,7 @@ public class Application {
         int hundred = num / 100;
         int ten = num % 100 / 10;
         int one = num % 100 % 10;
+        int[] result = {hundred, ten, one };
 
 
         while (true) {
@@ -24,8 +24,33 @@ public class Application {
             int num2 = input % 100 / 10;
             int num3 = input % 100 % 10;
 
+            int [] number = {num1, num2, num3};
+            int count=0;
+
+            for(int i=0; i<(result.length); i++) {
+                for(int j=0; j<(number.length); j++) {
+
+                    if(result[i] == number[j]) {
+                        count++;
+
+                        if(count>3) {
+                            count=3;
+                        }
+                    }
+                }
+
+            }
+
+            if(count >0) {
+                System.out.print(count + "볼 ");
+            }
+            else {
+
+            }
+
             if (hundred == num1 && ten == num2 && one == num3) {
                 System.out.println("3스트라이크 정답입니다.");
+                break;
             } else if (hundred == num1 && ten == num2) {
                 System.out.println("2스트라이크");
             } else if(hundred == num1 && one == num3) {
@@ -38,7 +63,11 @@ public class Application {
                 System.out.println("1스트라이크");
             } else if(one == num3) {
                 System.out.println("1스트라이크");
+            } else {
+                System.out.println("낫싱");
             }
+
+
         }
 
     }
